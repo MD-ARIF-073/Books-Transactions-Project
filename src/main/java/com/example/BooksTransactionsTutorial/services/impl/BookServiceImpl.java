2ebbbde9d6e89a3153ed5b22f5280e2b977910e8
Mohammad Arif Ishtiaq <arifsani73@gmail.com>
2024-02-06 13:03:28 +0600

    Test app without transaction and configure postgres db connection

--- src/main/java/com/example/BooksTransactionsTutorial/services/impl/BookServiceImpl.java
+++ src/main/java/com/example/BooksTransactionsTutorial/services/impl/BookServiceImpl.java
@@ -33,7 +33,7 @@ public class BookServiceImpl implements BookService {
      * There are easier ways to code this logic when you're not
      * demonstrating transactions -- take a look as Hibernate cascades!
      */
-    @Transactional
+
     @Override
     public Book save(final Book book) {
         if(null == book.getAuthor()) {
